0af1ebaeb4dee12c2c9c99de2aff105749ce743f
Trys <trysmudford@gmail.com>
2018-10-18 21:52:01 +0100

    A time picker

--- sw.js
+++ sw.js
@@ -5,67 +5,60 @@ with a few additional edits borrowed from Filament Group's. (https://www.filamen
 */
 
 (function() {
-  const version = 'v3';
+  const version = 'v4';
   const cacheName = ':countdown:';
 
   const staticCacheName = version + cacheName + 'static';
 
-  const staticAssets = [
-    '/',
-    '/countdown.mp3'
-  ];
+  const staticAssets = ['/', '/countdown.mp3'];
 
   function updateStaticCache() {
-    return caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(staticAssets.map(url => new Request(url, {credentials: 'include'})));
+    return caches.open(staticCacheName).then(cache => {
+      return cache.addAll(
+        staticAssets.map(url => new Request(url, { credentials: 'include' }))
+      );
     });
   }
 
   function trimCache(cacheName, maxItems) {
-    caches.open(cacheName)
-    .then(cache => {
-      cache.keys()
-      .then(keys => {
+    caches.open(cacheName).then(cache => {
+      cache.keys().then(keys => {
         if (keys.length > maxItems) {
-          cache.delete(keys[ 0 ])
-          .then(trimCache(cacheName, maxItems));
+          cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
         }
       });
     });
   }
 
   function clearOldCaches() {
-    return caches.keys()
-    .then(keys => {
-      return Promise.all(keys
-        .filter(key => key.indexOf(version) !== 0)
-        .map(key => caches.delete(key))
-       );
+    return caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key.indexOf(version) !== 0)
+          .map(key => caches.delete(key))
+      );
     });
   }
 
   self.addEventListener('install', event => {
-    event.waitUntil(updateStaticCache()
-      .then(() => self.skipWaiting())
-     );
+    event.waitUntil(updateStaticCache().then(() => self.skipWaiting()));
   });
 
   self.addEventListener('activate', event => {
-    event.waitUntil(clearOldCaches()
-      .then(() => self.clients.claim())
-     );
+    event.waitUntil(clearOldCaches().then(() => self.clients.claim()));
   });
 
   self.addEventListener('fetch', event => {
     const request = event.request;
     event.respondWith(
-      caches.match(request)
-        .then(res => {
-          return res || fetch(request)
+      caches.match(request).then(res => {
+        return (
+          res ||
+          fetch(request)
             .then(res => res)
-            .catch(console.error);
-        })
+            .catch(console.error)
+        );
+      })
     );
   });
 })();
